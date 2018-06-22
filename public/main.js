ec2936098a71d50b97211145055e5c9b54c9c2ee
Henry Cuffy <hcuffy_5@hotmail.com>
2018-06-22 18:13:42 +0200

    adding graph

--- public/main.js
+++ public/main.js
@@ -4,7 +4,34 @@ $(document).ready(function() {
 		function(data) {
 			const dataset = data.data;
 
-			document.write(dataset);
+			const w = 1000;
+			const h = 500;
+
+			const svg = d3
+				.select('body')
+				.append('svg')
+				.attr('width', w)
+				.attr('height', h);
+
+			svg
+				.selectAll('rect')
+				.data(dataset)
+				.enter()
+				.append('rect')
+				.attr('x', (d, i) => i * 30)
+				.attr('y', d => d[0])
+				.attr('width', 25)
+				.attr('height', d => d[1])
+				.attr('fill', 'navy');
+
+			svg
+				.selectAll('text')
+				.data(dataset)
+				.enter()
+				.append('text')
+				.text(d => d)
+				.attr('x', (d, i) => i * 30)
+				.attr('y', (d, i) => h - 3 * d - 3);
 		}
 	);
 });
